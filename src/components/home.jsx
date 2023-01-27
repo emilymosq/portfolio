20cdbb581e7800fce06dfd8bfe0f4d64791ef5c6
emilymsq <emilymosq4@gmail.com>
2023-01-27 15:40:13 +0100

    gh-pages

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -18,11 +18,11 @@ export default function Home() {
           <p>I like to bring ideas to life with code.</p>
           <h3>Junior FullStack Developer</h3>
           <div className="home__boxBtn">
-            <Link to="portfolio" smooth={true} offset={-100} duration={2000} className="btn home__btn">Check out my work!</Link>
+            <Link to="portfolio" smooth={true} offset={-100} duration={1000} className="btn home__btn">Check out my work!</Link>
           </div>
         </div>
         <div className="scroll__box">
-          <Link to="about" smooth={true} offset={-100} duration={2000} id="scroll__btn"></Link>
+          <Link to="about" smooth={true} offset={-100} duration={1000} id="scroll__btn"></Link>
         </div>
       </motion.div>
     </>
